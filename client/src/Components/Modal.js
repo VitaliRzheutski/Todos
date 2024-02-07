bc8f1a3b74c35aafa4f506e0741e5641b7d7b52d
VitaliRzheutski <vitalrzheutsky@gmail.com>
2024-02-07 17:11:27 -0500

    Created Modal component with styles

--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,6 +1,44 @@
 const Modal = () => {
+
+    const mode = 'edit'
+    const handleChange = () => {
+        console.log('Changing')
+    }
+
     return (
-        <div>
+        <div className="overlay">
+            <div className="modal">
+                <div className="form-title-container">
+                    <h3> Let's {mode} you task</h3>
+                    <button>X</button>
+                </div>
+
+                <form>
+                    <input
+                        required
+                        maxLength={30}
+                        placeholder="Your task goes here"
+                        name="title"
+                        value={""}
+                        onChange={handleChange}
+                    />
+                    <br />
+                    <label for="range">Drag to select current progress</label>
+                    <input
+                        required
+                        type="range"
+                        id="range"
+                        min="0"
+                        max="100"
+                        name="progress"
+                        value={""}
+                        onChange={handleChange}
+                    />
+                    <input className={mode} type="submit" />
+                </form>
+
+
+            </div>
         </div>
     );
 }
